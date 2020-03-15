870800deff0f4b55539514fa5d68f0de0e024ff3
Jackmt9 <jackmt9@gmail.com>
2020-03-15 18:09:43 -0400

    Done.

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,23 @@
 class Animal
 
+    attr_accessor :nickname
+    attr_reader :species, :weight, :zoo
+    @@all = []
+
+    def initialize(species, weight, nickname, zoo)
+        @species = species
+        @weight = weight
+        @nickname = nickname
+        @zoo = zoo
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.find_by_species
+        @@all.select{|animal| animal.species == self.species}
+    end
+
 end
